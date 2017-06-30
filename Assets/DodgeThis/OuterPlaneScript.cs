39a544332e1f2ffe7062b2b3928a7cf1c84ad4e3
Angus Yuen <angus.yuen29@gmail.com>
2017-06-30 19:50:56 +1000

    Added outerwalls, need to now add AI

---
+++ Assets/DodgeThis/OuterPlaneScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class OuterPlaneScript : MonoBehaviour {
+	public ThirdPersonCharacter thePlayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				
+	}
+
+	void OnTriggerEnter(Collider other) {
+		// resets the position of the player if it is the player
+		// who went onto the lava
+		if (other.gameObject.name == "ThirdPersonController") {
+			thePlayer.transform.position = new Vector3 (0.0f, 3.0f, 0.0f);
+		}
+	}
+}
