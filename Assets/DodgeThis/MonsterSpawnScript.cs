5ae7a31584f3a47d3d0f820095b5d75b55d2725e
Angus Yuen <angus.yuen29@gmail.com>
2017-07-01 22:23:56 +1000

    Added monster script, and edited spawning locations

---
+++ Assets/DodgeThis/MonsterSpawnScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawnScript : MonoBehaviour {
+	public GameObject zombear;
+	public GameObject zombunny;
+	public GameObject hellelephant;
+	public int thisWallID;	// 0, 1, 2, 3
+
+	/*
+	 * Spawn attributes
+	 */
+	private ArrayList allMonsters;	// all monsters at this wall
+	private int max_monsters;	// max number of monsters at this wall
+	public float minWallX;
+	public float maxWallX;
+	public float minWallZ;
+	public float maxWallZ;
+
+	private const float SPAWN_HEIGHT = 1.5f;
+
+	/* This is the nth wall
+	 * Monster spawns from nth wall
+	 * and makes its way to the opposite wall
+	 * (n + 2) % 4
+	 */
+	private const int NORTH_WALL = 0;
+	private const int EAST_WALL = 1;
+	private const int SOUTH_WALL = 2;
+	private const int WEST_WALL = 3;
+
+	// Use this for initialization
+	void Start () {
+		allMonsters = new ArrayList ();
+		max_monsters = Random.Range (5, 15);
+	
+		for (int i = 0; i < max_monsters; i++) {
+			float spawnLocationX = Random.Range (minWallX, maxWallX);
+			float spawnLocationZ = Random.Range (minWallZ, maxWallZ);
+			Quaternion spawnRotation = Quaternion.identity;
+
+			spawnRotation = setSpawnRotation (spawnRotation);
+
+			GameObject monster = (GameObject) Instantiate(zombunny, new Vector3(spawnLocationX, SPAWN_HEIGHT, spawnLocationZ), spawnRotation);
+			monster.transform.parent = gameObject.transform;
+			allMonsters.Add (monster);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	Quaternion setSpawnRotation(Quaternion spawnRotation) {
+		if (thisWallID == NORTH_WALL)
+			spawnRotation = Quaternion.Euler (new Vector3 (0.0f, 180f, 0.0f));
+
+		if (thisWallID == EAST_WALL)
+			spawnRotation = Quaternion.Euler (new Vector3 (0.0f, -90.0f, 0.0f));
+
+		if (thisWallID == SOUTH_WALL)
+			spawnRotation = Quaternion.Euler (new Vector3 (0.0f, 0.0f, 0.0f));
+
+		if (thisWallID == WEST_WALL)
+			spawnRotation = Quaternion.Euler(new Vector3(0.0f, 90.0f, 0.0f));
+
+		return spawnRotation;
+	}
+}
---
+++ Assets/DodgeThis/ZomBunnyScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZomBunnyScript : MonoBehaviour {
+	private float delay;		// this is the delay before start walking
+	private int WALL_START;	// this is the wall id the AI begins from
+
+	private float time;		// used to calculate elapsed time
+	private float speed;
+
+	/* This is the nth wall
+	 * Monster spawns from nth wall
+	 * and makes its way to the opposite wall
+	 * (n + 2) % 4
+	 */
+	private const int NORTH_WALL = 0;
+	private const int EAST_WALL = 1;
+	private const int SOUTH_WALL = 2;
+	private const int WEST_WALL = 3;
+
+	// Use this for initialization
+	void Start () {
+		delay = Random.Range (0.0f, 1.0f) * 1000;	// * 1000 convert to milliseconds
+		time = Time.time;
+		speed = 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float elapsedTime = Time.time;
+
+		if (elapsedTime - time >= delay) {
+			float posX = this.transform.position.x;
+			float posY = this.transform.position.y;
+			float posZ = this.transform.position.z;
+			// start walking
+			if (WALL_START == NORTH_WALL) {
+				// decrement z
+				this.transform.position.Set(posX, posY, posZ - speed);
+			}
+
+			if (WALL_START == SOUTH_WALL) {
+				// increment z
+				this.transform.position.Set(posX, posY, posZ + speed);
+			}
+
+			if (WALL_START == EAST_WALL) {
+				// decrement x
+				this.transform.position.Set(posX - speed, posY, posZ);
+			}
+
+			if (WALL_START == WEST_WALL) {
+				// increment x
+				this.transform.position.Set(posX + speed, posY, posZ);
+			}
+		}
+	}
+}
