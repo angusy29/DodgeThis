b3db6240089ae7be503a8500ee9c5d4bc2abaa3e
Angus Yuen <angus.yuen29@gmail.com>
2017-07-02 13:38:55 +1000

    Added main menu, escape menu, can transition between each menu and added escape button functionality, however upon quitting the game, the old game is resumed, instead of destroying and creating a new instantiation of a game

---
+++ Assets/DodgeThis/ControlScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlScript : MonoBehaviour {
+	private GameObject mainMenu;
+	private GameObject game;
+	private GameObject escapeMenu;
+
+	// Use this for initialization
+	void Start () {
+		mainMenu = GameObject.Find ("DodgeThis").transform.FindChild ("MainMenuCanvas").gameObject;
+		game = GameObject.Find ("DodgeThis").transform.FindChild ("Game").gameObject;
+		escapeMenu = GameObject.Find ("DodgeThis").transform.FindChild ("EscapeMenuCanvas").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (escapeMenu.activeInHierarchy) {
+				game.SetActive (true);
+				escapeMenu.SetActive (false);
+			} else if (game.activeInHierarchy) {
+				escapeMenu.SetActive (true);
+				game.SetActive (false);
+			}
+		}
+	}
+}
